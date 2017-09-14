85f7f74282dd070a057554af90501b0445cac474
Nathan K Levis <levisnathan@gmail.com>
2017-09-14 22:23:01 +0000

    base files

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<? php
+     include 'inc/functions.php';
+?>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+        <meta charset="utf-8" />
+        <title>
+            
+        </title>
+    </head>  
+        <body>
+            <header>
+                <h1></h1>
+            </header>
+            <? php
+               play();
+            ?>
+        </body>
+    <footer>
+        
+    </footer>
+</html>
